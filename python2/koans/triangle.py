3aeeeb9354aef3fc8351791bf368b7b0f7be7bd7
Kevin Ho <keho98@gmail.com>
2014-02-16 21:45:24 -0800

    Finished triangle pt 2

--- python2/koans/triangle.py
+++ python2/koans/triangle.py
@@ -19,7 +19,9 @@
 #
 def triangle(a, b, c):
     # DELETE 'PASS' AND WRITE THIS CODE
-    if a == b and b == c: return 'equilateral'
+    if a <= 0 or b <= 0 or c <= 0: raise TriangleError()
+    elif a >= b + c or b >= a + c or c >= a + b: raise TriangleError()
+    elif a == b and b == c: return 'equilateral'
     elif a == b or b == c or a == c: return 'isosceles'
     else: return 'scalene'
 
